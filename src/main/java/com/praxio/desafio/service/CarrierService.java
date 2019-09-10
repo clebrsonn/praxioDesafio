e4d1c05bbda5ccc46815a51590028cfcf95dede8
clebrsonn@hotmail.com <W0rk5f1n3>
2019-09-10 07:06:49 -0300

    Metódo de criacao e atualização de Carrier tanto no service quanto no resource

--- src/main/java/com/praxio/desafio/service/CarrierService.java
+++ src/main/java/com/praxio/desafio/service/CarrierService.java
@@ -3,7 +3,6 @@ package com.praxio.desafio.service;
 import com.praxio.desafio.model.Carrier;
 import com.praxio.desafio.repository.CarrierRepository;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
